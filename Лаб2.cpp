98a2a8df4e91768ae6629e9998ce69a7fa90a6c3
Lina_Li <ilyinadother@gmail.com>
2020-08-25 00:10:22 +0300

    initial commit

---
+++ Лаб2.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+ void main()
+{
+	 int a;
+	 float x,y,b;
+	 float h = 0.2;
+
+	 cout << "for" << endl;
+	 for (a = 1; b <= 2; x = x + h)
+	 {
+		 y = cos(x) * exp(-x);
+		 cout << "y=" << y << endl;
+	 }
+
+	 cout << "while" << endl;
+	 a = 1;
+	 while (b <= 2)
+	 {
+		 y = cos(x) * exp(-x);
+		 x = x + h;
+		 cout << "y=" << y << endl;
+	 }
+
+	 cout << "do...while" << endl;
+	 a = 1;
+	 do
+	 {
+		 y = cos(x) * exp(-x);
+		 x = x + h;
+		 cout << "y=" << y << endl;
+	 } 
+	 while (b <= 2);
+
+
+	 system("pause");
+}
+
